cfb93d063e89af0ef785bb884d4de9d80b2fe9b3
dashakoryakovskaya <dasha_cat27@mail.ru>
2023-02-22 01:32:12 +0300

    message_handler for start (add_user) and text (+ - *_data)

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import threading
+
 import config
 import telebot
 import db
@@ -5,5 +7,49 @@ import db
 bot = telebot.TeleBot(config.token)
 
 
+def list_of_tuples_to_str(list_tup: list):
+    string = ''
+    for row in list_tup:
+        string += ' '.join(map(str, row)) + '\n'
+    return string
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет ✌️ Я - бот для отслеживания твоих финансов! \n"
+                                      "+sum - поступления \n"
+                                      "-sum - траты \n"
+                                      "user_data - информация о пользователях\n"
+                                      "incomes_data - информация о поступлениях\n"
+                                      "expenses_data - информация о тратах")
+    db.add_user(user_id=message.from_user.id, name=message.from_user.first_name)
+    # bot.send_message(message.chat.id, str(threading.current_thread().ident))
+
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    # bot.send_message(message.chat.id, str(threading.current_thread().ident))
+    # TODO: сделать это все через меню / кнопки, категории!!
+    if message.text[0] == '+':
+        db.add_incomes(user_id=message.from_user.id, name=message.from_user.username, date=message.date,
+                       sum=int(message.text[1:]), type='')
+    if message.text[0] == '-':
+        db.add_expenses(user_id=message.from_user.id, name=message.from_user.username, date=message.date,
+                        sum=int(message.text[1:]), type='')
+    # вся информация из таблицы по запросу имятаблицы_data
+    if message.text[-4:] == 'data':
+        bot.send_message(message.chat.id, message.text + ':\n' + list_of_tuples_to_str(db.sql_execute(sql="SELECT * "
+                                                                                                          "FROM " +
+                                                                                                          message.text[
+                                                                                                          :-5])))
+
+
+def main():
+    # TODO: добавить проверку соединения и тд
+    bot.polling()
+
+
 if __name__ == '__main__':
-    db.init_db()
+    # TODO: нужен ли тут flag_drop=True ?
+    db.init_db(flag_drop=True)
+    main()
